e3bd756b1dd1ab1143b44f41f8726d20cf57eafe
luizH <luizhgd01@gmail.com>
2025-09-30 14:56:46 -0300

    new 2 - 1

--- javascript/script.js
+++ javascript/script.js
@@ -19,6 +19,8 @@
 // console.log (4 % 4);   // Módulo
 // console.log (4 ** 4);  // Potenciação
 
+// console.log();
+
 //---------------------------------------//
 
 // Operadores de Comparação
@@ -30,13 +32,25 @@
 // console.log(10 == '10'); // iqualdade
 // console.log(10 === '10'); // estrita igualdade
 
+// console.log();
+
 //---------------------------------------//
 
 // Operadores de Atribuição
 
-var numero = 10; // atribuição
-numero += 5; // atribuição com adição de valor
-numero -= 5; // atibuição com subtração de valor
-numero *= 5; // atibuição com multiplicação de valor
-numero -= 5; // atibuição com divisão de valor
-console.log(numero);
+// var numero = 10; // atribuição
+// numero += 5; // atribuição com adição de valor
+// numero -= 5; // atibuição com subtração de valor
+// numero *= 5; // atibuição com multiplicação de valor
+// numero -= 5; // atibuição com divisão de valor
+
+// console.log(numero);
+
+//---------------------------------------//
+
+var idade = 18;
+// console.log(idade >= 18);
+
+if(idade = 18){
+    console.log('Você e maior de idade.')
+}
