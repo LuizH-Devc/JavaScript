5ec3bf8080a82a321e5a6026ca576199364b3231
luizH <luizhgd01@gmail.com>
2025-09-30 14:37:08 -0300

    new 1

---
+++ script.js
@@ -0,0 +1,31 @@
+//---------------------------------------//
+
+// var frase = 'Meu nome é Luiz Henrique.';
+// var idade = 22.03;
+// var ativo = false;
+// var contem = null;
+// var definido;
+
+// console.log();
+
+//---------------------------------------//
+
+// Operadores Aritméticos
+
+// console.log (4 + 4 );  // Adição
+// console.log (4 - 4);   // Subtração
+// console.log (4 * 4);   // Mutiplicação
+// console.log (4 / 4);   // Divisão
+// console.log (4 % 4);   // Módulo
+// console.log (4 ** 4);  // Potenciação
+
+//---------------------------------------//
+
+// Operadores de Comparação
+
+// console.log(10 > 5); // maior que
+// console.log(10 < 5); // menor que
+// console.log(10 >= 5); // maior ou iqual a que
+// console.log(10 <= 5); // menor ou iqual a que
+// console.log(10 == '10'); // iqualdade
+// console.log(10 === '10'); // estrita igualdade
